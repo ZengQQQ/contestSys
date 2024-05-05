779db25521b5516112e07890d7de1978667c03e6
Leidada <k82k0262@163.com>
2024-05-05 21:37:32 +0800

    RelationshipServe修改

---
+++ src/main/java/com/game/servlet/admin/RelationTeamUserInsert.java
@@ -0,0 +1,39 @@
+package com.game.servlet.admin;
+
+import com.game.serve.RelationshipServe;
+import com.game.utils.Result;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@WebServlet(value = "/admin/RelationTeamUserInsert")
+public class RelationTeamUserInsert extends HttpServlet {
+    RelationshipServe relation = new RelationshipServe();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        StringBuilder jsonBuilder = new StringBuilder();
+        String line;
+        try (BufferedReader reader = req.getReader()) {
+            while ((line = reader.readLine()) != null) {
+                jsonBuilder.append(line);
+            }
+        }
+        String jsonString = jsonBuilder.toString();
+        Result<String> result = relation.insertTeamRelation(jsonString);
+        String json = new Gson().toJson(result);
+        resp.setContentType("application/json");
+        resp.getWriter().write(json);
+        resp.getWriter().flush();
+    }
+}
