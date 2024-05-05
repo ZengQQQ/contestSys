d90a3f76f691fcdefeb80e0a021162e6b17cbcfa
Leidada <k82k0262@163.com>
2024-05-06 01:28:06 +0800

    m,p,t三表处理

--- src/main/java/com/game/serve/ProjectService.java
+++ src/main/java/com/game/serve/ProjectService.java
@@ -1,6 +1,5 @@
 package com.game.serve;
 
-import com.game.bean.PageBean;
 import com.game.dao.ProjectDao;
 import com.game.domain.Project;
 import com.game.utils.Result;
@@ -11,31 +10,30 @@ public class ProjectService {
 
     private static final ProjectDao projectDao = new ProjectDao();
 
-
     public Result<String> insert(Project project) {
-
-        // 参数检查
-
-        // 查询重复
         List<Project> exited = projectDao.query(project,-1,-1);
         if (!exited.isEmpty()){
-
-            Result<String> fail = Result.fail("已存在","");
-            return fail;
+            return Result.fail("更新失败","项目已存在");
         }
-        // 添加
         boolean inserted = projectDao.insert(project);
         if (inserted){
-            Result<String> success = Result.success("");
-            return success;
+            return Result.success("");
         }
-
-        // 其它问题
-        return Result.fail("添加失败，稍后重试","");
-
+        return Result.fail("更新失败","");
     }
 
-
-
+    public Result<String> update(Project project) {
+        Project tar = new Project();
+        tar.setP_id(project.getP_id());
+        List<Project> exited = projectDao.query(tar,-1,-1);
+        if (exited.isEmpty()){
+            return Result.fail("更新失败","没有该项目");
+        }
+        int updated = projectDao.update(project,tar);
+        if (updated == 0){
+            return Result.fail("更新失败","更新失败");
+        }
+        return Result.success("更新成功");
+    }
 
 }
--- src/main/java/com/game/serve/TeamServe.java
+++ src/main/java/com/game/serve/TeamServe.java
@@ -1,9 +1,38 @@
 package com.game.serve;
 
 import com.game.dao.TeamDao;
+import com.game.domain.Team;
+import com.game.utils.Result;
 
-public class TeamServe extends TeamDao {
+import java.util.List;
 
+public class TeamServe{
 
+    private static final TeamDao teamDao = new TeamDao();
 
+    public Result<String> insert(Team team) {
+        List<Team> exited = teamDao.query(team,-1,-1);
+        if (!exited.isEmpty()){
+            return Result.fail("更新失败","团队已存在");
+        }
+        boolean inserted = teamDao.insert(team);
+        if (inserted){
+            return Result.success("");
+        }
+        return Result.fail("更新失败","");
+    }
+
+    public Result<String> update(Team team) {
+        Team tar = new Team();
+        tar.setT_id(team.getT_id());
+        List<Team> exited = teamDao.query(tar,-1,-1);
+        if (exited.isEmpty()){
+            return Result.fail("更新失败","没有该团队");
+        }
+        int updated = teamDao.update(team,tar);
+        if (updated == 0){
+            return Result.fail("更新失败","更新失败");
+        }
+        return Result.success("更新成功");
+    }
 }
