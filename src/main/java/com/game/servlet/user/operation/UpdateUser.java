59d2f3c0bde7e0d99b67cde08c8ba837732d8bcf
Leidada <k82k0262@163.com>
2024-05-07 13:47:44 +0800

    Userupdate接口

---
+++ src/main/java/com/game/servlet/user/operation/UpdateUser.java
@@ -0,0 +1,47 @@
+package com.game.servlet.user.operation;
+
+import com.alibaba.fastjson2.JSON;
+import com.game.domain.Team;
+import com.game.domain.User;
+import com.game.serve.UserService;
+import com.game.utils.Result;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(value = "/user/UpdateUser")
+public class UpdateUser extends HttpServlet {
+    UserService relation = new UserService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Enumeration<String> parameterNames = req.getParameterNames();
+        Map<String, Object> paramMap = new HashMap<>();
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            String paramValue = req.getParameter(paramName);
+            paramMap.put(paramName, paramValue);
+        }
+
+        String jsonString = JSON.toJSONString(paramMap);
+        User user = JSON.parseObject(jsonString, User.class);
+
+        Result<String> result = relation.update(user);
+        String json = JSON.toJSONString(result);
+        resp.setContentType("application/json");
+        resp.getWriter().write(json);
+        resp.getWriter().flush();
+    }
+}
