fd79e628ac48810fca77a2d3811a9ea1a92ad39d
Leidada <k82k0262@163.com>
2024-05-07 12:45:39 +0800

    添加project和team关闭接口

---
+++ src/main/java/com/game/servlet/user/operation/CloseProject.java
@@ -0,0 +1,49 @@
+package com.game.servlet.user.operation;
+
+import com.alibaba.fastjson2.JSON;
+import com.game.domain.Project;
+import com.game.serve.ProjectService;
+import com.game.utils.Result;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(value = "/user/CloseProject")
+public class CloseProject extends HttpServlet {
+    ProjectService relation = new ProjectService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Enumeration<String> parameterNames = req.getParameterNames();
+        Map<String, Object> paramMap = new HashMap<>();
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            String paramValue = req.getParameter(paramName);
+            paramMap.put(paramName, paramValue);
+        }
+
+        String jsonString = JSON.toJSONString(paramMap);
+        Project project = JSON.parseObject(jsonString, Project.class);
+        Project tar = new Project();
+        tar.setP_id(project.getP_id());
+        tar.setP_status(1);
+
+        Result<String> result = relation.update(tar);
+        String json = JSON.toJSONString(result);
+        resp.setContentType("application/json");
+        resp.getWriter().write(json);
+        resp.getWriter().flush();
+    }
+}
