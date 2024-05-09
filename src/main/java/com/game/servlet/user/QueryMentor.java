9233a2155ee9fd17a0d26fa900745dbcbdfe6a74
BBBBlightyear~ <1993673178@qq.com>
2024-05-09 17:42:14 +0800

    joinedstall debug

--- src/main/java/com/game/servlet/user/QueryMentor.java
+++ src/main/java/com/game/servlet/user/QueryMentor.java
@@ -1,29 +1,25 @@
 package com.game.servlet.user;
 
+
 import com.alibaba.fastjson2.JSON;
 import com.game.bean.PageBean;
-import com.game.domain.Stall;
-import com.game.domain.StallTeamMessage;
-import com.game.domain.User;
-import com.game.domain.fixDomain.StallFix;
+import com.game.domain.Mentor;
 import com.game.serve.QueryControlServe;
 import com.game.utils.CurPage;
 import com.game.utils.Result;
-import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(value = "/user/queryStall")
-public class QueryStall extends HttpServlet {
+@WebServlet(value = "/user/queryMentor")
+public class QueryMentor extends HttpServlet {
     QueryControlServe query = new QueryControlServe();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -36,6 +32,7 @@ public class QueryStall extends HttpServlet {
         resp.setContentType("application/json;charset=UTF-8");
 
         Enumeration<String> parameterNames = req.getParameterNames();
+
         Map<String, Object> paramMap = new HashMap<>();
 
         while (parameterNames.hasMoreElements()) {
@@ -44,8 +41,8 @@ public class QueryStall extends HttpServlet {
             paramMap.put(paramName, paramValue);
         }
         String jsonString = JSON.toJSONString(paramMap);
-        Stall stall = JSON.parseObject(jsonString, Stall.class);
-        stall.setSt_status(0);
+        Mentor stall = JSON.parseObject(jsonString, Mentor.class);
+        stall.setM_status(0);
          Integer currentPage;
         try{
             currentPage=JSON.parseObject(jsonString, CurPage.class).getCurrentPage();
@@ -53,7 +50,7 @@ public class QueryStall extends HttpServlet {
             currentPage =1;
         }
 
-        Result<PageBean<StallFix>> responseData =query.queryPage(currentPage,stall);
+        Result<PageBean<Mentor>> responseData =query.queryPage(currentPage,stall);
         String json = JSON.toJSONString(responseData);
         resp.setContentType("application/json");
         resp.getWriter().write(json);
--- src/main/java/com/game/servlet/user/QueryTeam.java
+++ src/main/java/com/game/servlet/user/QueryTeam.java
@@ -1,29 +1,26 @@
 package com.game.servlet.user;
 
+
 import com.alibaba.fastjson2.JSON;
 import com.game.bean.PageBean;
-import com.game.domain.Stall;
-import com.game.domain.StallTeamMessage;
-import com.game.domain.User;
-import com.game.domain.fixDomain.StallFix;
+import com.game.domain.Team;
+import com.game.domain.fixDomain.TeamFix;
 import com.game.serve.QueryControlServe;
 import com.game.utils.CurPage;
 import com.game.utils.Result;
-import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(value = "/user/queryStall")
-public class QueryStall extends HttpServlet {
+@WebServlet(value = "/user/queryTeam")
+public class QueryTeam extends HttpServlet {
     QueryControlServe query = new QueryControlServe();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -36,6 +33,7 @@ public class QueryStall extends HttpServlet {
         resp.setContentType("application/json;charset=UTF-8");
 
         Enumeration<String> parameterNames = req.getParameterNames();
+
         Map<String, Object> paramMap = new HashMap<>();
 
         while (parameterNames.hasMoreElements()) {
@@ -44,8 +42,8 @@ public class QueryStall extends HttpServlet {
             paramMap.put(paramName, paramValue);
         }
         String jsonString = JSON.toJSONString(paramMap);
-        Stall stall = JSON.parseObject(jsonString, Stall.class);
-        stall.setSt_status(0);
+        Team stall = JSON.parseObject(jsonString, Team.class);
+        stall.setT_status(0);
          Integer currentPage;
         try{
             currentPage=JSON.parseObject(jsonString, CurPage.class).getCurrentPage();
@@ -53,7 +51,8 @@ public class QueryStall extends HttpServlet {
             currentPage =1;
         }
 
-        Result<PageBean<StallFix>> responseData =query.queryPage(currentPage,stall);
+
+        Result<PageBean<TeamFix>> responseData =query.queryPage(currentPage,stall);
         String json = JSON.toJSONString(responseData);
         resp.setContentType("application/json");
         resp.getWriter().write(json);
